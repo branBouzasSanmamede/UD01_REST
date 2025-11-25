6dae621b77728179df7a300143e7168e3375e92f
branBouzasSanmamede <bran.bouzas@iessanmamede.com>
2025-11-25 11:37:12 +0000

    Detallitos

--- t05/index.py
+++ t05/index.py
@@ -1,7 +1,12 @@
 from t05.e1.index import main as e1
-from t05.e2.index import main as e2
 from t05.e3.index import main as e3
 from utils.util_menus import ejecutar_menu
+import platform
+
+if platform.system() == "Windows":
+    from t05.e2.index import main as e2
+else:
+    e2 = None
 
 menu = [
     (1, "Información del sistema", e1),
@@ -10,7 +15,8 @@ menu = [
 ]
 
 def main():
-    ejecutar_menu(menu)
+    menu_filtrado = [(num, texto, func) for num, texto, func in menu if func is not None]
+    ejecutar_menu(menu_filtrado)
 
 if __name__ == "__main__":
     main()
